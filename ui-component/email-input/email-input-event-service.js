878f02b0de8baa332fd5496d742d0876a94169dc
Aleksandr Cherepanov <a.cherepanov@xsolla.com>
2020-05-24 21:06:31 +0500

    return events for ie11

--- ui-component/email-input/email-input-event-service.js
+++ ui-component/email-input/email-input-event-service.js
@@ -1,8 +1,16 @@
 const createEvent = (eventId, isBubbled, detail = {}) => {
-    return new CustomEvent(eventId, {
-        bubbles: isBubbled,
-        detail: detail
-    })
+    let event;
+    if(typeof(Event) === "function") {
+        event = new CustomEvent(eventId, {
+            bubbles: isBubbled,
+            detail: detail
+        })
+    } else {
+        event = document.createEvent("CustomEvent");
+        event.initCustomEvent(eventId, isBubbled, true, detail);
+    }
+
+    return event;
 }
 
 export const dispatchEvent = (elementDispatcher, eventId, details = {}) => {
